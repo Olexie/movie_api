64d452c4afa6d0a97b4a0b12a606f774174288ab
Olexie <116206464+Olexie@users.noreply.github.com>
2023-04-05 20:08:52 +0100

    add list of movies

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require ('express');
+const app = express();
+
+let marvel =[
+    {
+        title: 'Iron Man',
+        director: 'Jon Favreau',
+        releaseUs: ' May 2008'
+    },
+    {
+        title: 'The Incredible Hulk',
+        director:'Louis Leterrier',
+        releaseUs:'June 2008'
+    },
+    {
+        title: 'Iron Man 2',
+        director: 'Jon Favreau',
+        releaseUs: 'May 2010'
+    },
+    {
+        title:'Thor',
+        director:'Kenneth Branagh',
+        releaseUs:'May 2011'
+    }, 
+    {
+        title:'Captain America: The First Avenger',
+        director:'Joe Johnston',
+        releaseUs:'July 2011'
+    },
+    {
+        title:'The Avengers',
+        director:'Joss Whedon',
+        releaseUs:'May 2012'
+    }, 
+    {
+        title:'Iron Man 3',
+        director:'Shane Black',
+        releaseUs:'May 2013'
+    },
+    {
+        title:'Thor: The Dark World',
+        director:'Alan Taylor',
+        releaseUs:'November 2013'
+    },
+    {
+        title: 'Captain America: The Winter Soldier',
+        director:'Antony and Joe Russo',
+        releaseUs:'April 2014'
+    },
+    {
+        title: 'Avengers: Age of Ultron',
+        director:'Joss Whedon',
+        releaseUs:'May 2015'
+    }
+];
