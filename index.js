e956626e006cfcd19dbc4932c37be7ce37765241
Olexie <116206464+Olexie@users.noreply.github.com>
2023-04-11 14:55:49 +0100

    fixed syntax error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express');
  const morgan = require ('morgan');
  const fs = require('fs');
- const path = require('path';)
+ const path = require('path');
 
 const app = express();
 
