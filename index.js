4e64340094a1fb28e39eb717b88b83c0fc6b3c62
Olexie <116206464+Olexie@users.noreply.github.com>
2023-04-16 20:47:17 +0100

    finished changes for mongoose integration

--- index.js
+++ index.js
@@ -1,342 +1,225 @@
-const express = require ('express');
-const morgan = require ('morgan');
-const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser');
-const uuid = require('uuid');
-const mongoose = require('mongoose');
-const models = require('./models.js')
-const { unsafeStringify } = require('stringify');
+const express = require("express");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+const { unsafeStringify } = require("stringify");
+
+// Importing the models
+const Movies = Models.Movie;
+const Users = Models.User;
+//const Universe = Models.Universe;
+//const Directors = Models.Director;
 
 const app = express();
 
-app.use(morgan('common'));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-let users=[
-    {
-        id:'1',
-        name:'Jane',
-        username:'jane1',
-        password:'favouritemovie1',
-        email:'janej@gmail.com',
-        birrthday:'1990-03-07',
-        favouriteMovies:[]
-
-    },
-    {
-        id:'2',
-        name:'Kokos',
-        username:'coconut',
-        password:'123coconut',
-        email:'iamkokos@gmail.com',
-        birthday:'1988-01-05',
-        favouriteMovies:['Thor', 'Iron Man']
-    },
-    {
-        id:'3',
-        name:'Miso',
-        username:'misoon',
-        password:'ilovefood',
-        email:'miso@gmail.com',
-        birthday:'2010-07-03',
-        favouriteMovies:[]
-    },
-    {
-        id:'4',
-        name:'Marsik',
-        username:'mainecoon',
-        password:'coonsarethebest1',
-        email:'mars@gmail.com',
-        birthday:'1998-09-14',
-        favouriteMovies:[]
-    },
-    {
-        id:'5',
-        name:'Mochi',
-        username:'mochiball',
-        password:'rtj34f66',
-        email:'mochi@gmail.com',
-        birthday:'2004-07-07',
-        favouriteMovies:[]
-    }
-    
-
-
-];
-
-let movies =[
-    
-    {
-        title: 'Iron Man',
-        director: {
-           name: 'Jon Favreau',
-           bio:'is an American actor and filmmaker. As an actor',
-           birth: 'October 19, 1966'
-    },
-        releaseUs: 'May 2008',
-        universe: {
-            name: 'Marvel',
-            description: 'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios.'
-        }
-    },
-    {
-        title: 'The Incredible Hulk',
-        director:{
-          name:  'Louis Leterrier',
-          bio:' is a French film director and producer. Best known for his work in action films',
-          birth:'17 June 1973 '
-        },
-        releaseUs:'June 2008',
-        universe: {
-            name: 'Marvel',
-            description: 'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios.'
-        }   
-     },
-    {
-        title: 'Iron Man 2',
-        director: {
-            name: 'Jon Favreau',
-            bio:'is an American actor and filmmaker. As an actor',
-            birth: 'October 19, 1966'},
-        releaseUs: 'May 2010',
-        universe: {
-            name: 'Marvel',
-            description: 'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios.'
-        }
-    },
-    {
-        title:'Thor',
-        director:{
-            name:'Kenneth Branagh',
-            bio:'is a British actor and filmmaker.',
-            birth:'10 December 1960'
-        },
-        releaseUs:'May 2011',
-        universe: {
-            name: 'Marvel',
-            description: 'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios.'
-        }
-    }, 
-    {
-        title:'Captain America: The First Avenger',
-        director:{
-          name:  'Joe Johnston',
-          bio: 'is an American film director, producer, writer, and visual effects artist.',
-          birth: 'May 13, 1950'
-        },
-        releaseUs:'July 2011',
-        universe: {
-            name: 'Marvel',
-            description: 'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios.'
-        }
-    },
-    {
-        title:'The Avengers',
-        director:{
-           name: 'Joss Whedon',
-           bio:'is an American filmmaker, composer, and comic book writer.',
-           birth: 'June 23, 1964'
-        },
-        releaseUs:'May 2012',
-        universe: {
-            name: 'Marvel',
-            description: 'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios.'
-        }
-    }, 
-    {
-        title:'Iron Man 3',
-        director:{
-            name:'Shane Black',
-            bio:'is an American filmmaker and actor ',
-            birth:'December 16, 1961'
-        },
-        releaseUs:'May 2013',
-        universe: {
-            name: 'Marvel',
-            description: 'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios.'
-        }
-    },
-    {
-        title:'Thor: The Dark World',
-        director:{
-            name:'Alan Taylor',
-            bio:'is an American filmmaker and actor ',
-            birth:'October 24, 1958'
-        },
-        releaseUs:'November 2013',
-        universe: {
-            name: 'Marvel',
-            description: 'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios.'
-        }
-    },
-    {
-        title: 'Captain America: The Winter Soldier',
-        director:{
-          name:  'Antony and Joe Russo',
-          bio:'collectively known as the Russo brothers (ROO-so), are American directors, producers, and screenwriters. ',
-          birth: 'February 3, 1970,July 18, 1971'
-        },
-        releaseUs:'April 2014',
-        universe: {
-            name: 'Marvel',
-            description: 'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios.'
-        }
-    },
-    {
-        title: 'Avengers: Age of Ultron',
-        director:{
-            name: 'Joss Whedon',
-            bio:'is an American filmmaker, composer, and comic book writer.',
-            birth: 'June 23, 1964'
-         },
-        releaseUs:'May 2015',
-        universe: {
-            name: 'Marvel',
-            description: 'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios.'
-        }
-    },
-    {
-        title: 'Wonder Woman',
-        director:{
-            name:'Patricia Lea Jenkins',
-            bio:'is an American film director, screenwriter, and producer.',
-            birth: 'July 24, 1971'
-         },
-        releaseUs:'May 2017',
-        universe: {
-            name: 'DC',
-            description: 'is an American media franchise and shared universe centered on a series of superhero films and television series produced by DC Studios and distributed by Warner Bros. Pictures.'
-        }
-    }
-
-];
-
+mongoose
+  .connect("mongodb://127.0.0.1:27017/my_movie_api")
+  .catch((error) => handleError(error));
 
 //CREATE
-app.post('/users',(req, res)=>{
-    const newUser=req.body;
+app.post("/users", (req, res) => {
+  Users.findOne({
+    username: req.body.username,
+  }) /* checking to see if the username already exists by querying the Users model */
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.username + "already exists");
+      } else {
+        Users.create({
+          /* if user doesn't exist then Users.create mongoose' CREATE command is used to create the new user */
+          name: req.body.name,
+          username: req.body.username,
+          password: req.body.password,
+          email: req.body.email,
+          birthday: req.body.birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          }) /* call back sending a response back to the client with the status code and the document "user", letting them know the transaction is completed */
+          .catch((error) => {
+            /* error handling */
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
+});
 
-    if (newUser.name){
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else{
-        res.status(400).send('Name required')
-    }
-})
+// Get all users
+app.get("/users", (req, res) => {
+  Users.find() /* Querying users model and grabbing all data from users collection */
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
-app.put('/users/:id', (req, res)=>{
-    const {id} =req.params;
-    const updateUser = req.body;
+// Get information about a specific user by username
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({ username: req.params.Username })
+    .then((users) => {
+      /* sending the response with user data back to the client */
+      res.json(users);
+    })
+    .catch((err) => {
+      /* error handling */
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
-    let user = users.find(users => users.id ==id);
+// CREATE - Allow users to add a movie to their list of favorites
+app.post("/users/:Username/movies/:MovieID", (req, res) => {
+  const promise = Users.findOneAndUpdate(
+    { username: req.params.Username },
+    {
+      $push: {
+        favouriteMovies: req.params.MovieID,
+      },
+    },
+    { new: true }
+  ).exec();
 
-    if(user){
-        user.name = updateUser.name;
-        res.status(200).json(user);
+  promise.then((updatedUser) => {
+    res.json(updatedUser);
+  });
+});
+
+// UPDATE User info
+app.put("/users/:Username", (req, res) => {
+  const promise = Users.findOneAndUpdate(
+    { username: req.params.Username },
+    {
+      $set: {
+        username: req.body.username,
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        birthday: req.body.birthday,
+      },
+    },
+    { new: true }
+  ) // This line makes sure that the updated document is returned
+    .exec();
+
+  promise.then((updatedUser) => {
+    res.json(updatedUser);
+  });
+});
+
+// DELETE - Allow users to remove a movie from their list of favorites
+app.delete("/users/:Username/movies/:MovieID", (req, res) => {
+  const promise = Users.findOneAndUpdate(
+    { username: req.params.Username },
+    {
+      $pull: {
+        favouriteMovies: req.params.MovieID,
+      } /* using $pull to remove movieID from favorite movies array. */,
+    },
+    { new: true }
+  ) /* makes sure updated data is returned */
+    .exec();
+
+  promise.then((updatedUser) => {
+    res.json(updatedUser);
+  });
+});
+
+// Delete a user by username
+app.delete("/users/:Username", (req, res) => {
+  const promise = Users.findOneAndRemove({
+    username: req.params.Username,
+  }).exec();
+
+  promise.then((user) => {
+    /* checking if the document exists, if it does it gets deleted, if not it responds with was not found */
+    if (!user) {
+      res.status(400).send(req.params.Username + " was not found");
     } else {
-        res.status(400).send('user not found')
+      res.status(200).send(req.params.Username + " was deleted.");
     }
-})
+  });
+});
 
-app.post('/users/:id/:movieTitle', (req, res)=>{
-    const {id, movieTitle} =req.params;
+// READ - Get list of all movies
+app.get("/movies", (req, res) => {
+  Movies.find() /* Querying movies model and grabbing all data from movies collection */
+    .then((movies) => {
+      res.status(200).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+// Find movie by title
+app.get("/movies/:Title", (req, res) => {
+  Movies.findOne({ title: req.params.Title })
+    .then((movies) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      /* error handling */
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
-    let user =users.find(users =>users.id==id);
+// Find movie by universe
+app.get("/movies/universe/:Universe", (req, res) => {
+  Movies.findOne({ "universe.name": req.params.Universe })
+    .then((movies) => {
+      res.json(movies.universe);
+    })
+    .catch((err) => {
+      /* error handling */
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
-    if(user){
-        user.favouriteMovies.push(movieTitle);
-        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);;
-    }else{
-        res.status(400).send('no user found')
-    }
-})
-
-app.delete('/users/:id/:movieTitle', (req, res)=>{
-    const {id, movieTitle} = req.params;
-
-    let user =users.find(user=>user.id==id);
-
-    if (user){
-        user.favouriteMovies=user.favouriteMovies.filter(title =>title !== movieTitle);
-        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);;
-    }
-})
-
-app.delete('/users/:id',(req,res)=>{
-    const {id} =req.params;
-
-    let user = users.find(users => users.id == id);
-
-    if (user){
-        users = users.filter(users => users.id !== id);
-        res.status(200).send(`user ${id} has been removed`);;
-    } else{
-        res.status(400).send('user not found')
-    }
-})
+// Find data about director by name
+app.get("/movies/director/:DirectorName", (req, res) => {
+  Movies.findOne({ "director.name": req.params.DirectorName })
+    .then((movies) => {
+      res.json(movies.director);
+    })
+    .catch((err) => {
+      /* error handling */
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 //GET request
-app.get ('/',(req, res) =>{
-    res.send ('Welcome to the superheroe universee!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the superheroe universe!");
 });
 
-//app.get('/movies', (req, res) =>{
-//  res.json(marvel);
-//});
-
-app.get('/documentation', (req, res)=>{
-res.sendFile('public/documentation.html', {root: _dirname});
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/documentation.html", { root: _dirname });
 });
 
-app.get('/movies', (req, res) => {
-    res.status(200).json(movies);
-})
-
-app.get('/users', (req, res) => {
-    res.status(200).json(users);
-})
-
-app.get('/movies/:Title', (req, res)=>{
-    const{Title}=req.params;
-    const movie = movies.find(movies =>movies.title===Title);
-
-    if (movie){
-        res.status(200).json(movie);
-    } else{
-        res.status(400).send('movie not found')
-    }
-})
-
-app.get('/movies/universe/:universeName',(req, res)=>{
-    const {universeName} =req.params;
-    const universe = movies.find(movies =>movies.universe.name===universeName).universe;
-    if (universe){
-        res.status(200).json(universe);
-    } else{
-        res.status(400).send('not found')
-    }
-})
-
-app.get('/movies/director/:directorName', (req, res) => {
-    const{directorName} =req.params;
-    const director =movies.find(movies => movies.director.name===directorName).director;
-
-    if(director){
-        res.status(200).json(director);
-    }else{
-        res.status(400).send('director not found')
-    }
-})
-
-
-app.use((err, req, res, next)=>{
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
-app.listen(8080, () =>{
-    console.log('Your app is listening on port 8080.');
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
 });
