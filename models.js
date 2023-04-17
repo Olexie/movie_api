eea85ccc337ad4a58bb6903d8366a643b0635d18
Olexie <116206464+Olexie@users.noreply.github.com>
2023-04-17 14:56:09 +0100

    add models

--- models.js
+++ models.js
@@ -1,31 +1,35 @@
-const { default: mongoose } = require('mongoose');
+const { default: mongoose } = require("mongoose");
 
 let movies_schema = mongoose.Schema({
-    title: {type: String, required: true},
-    releaseUs: {type: String, required: true},
-    universe: {
-        name: String,
-        description: String
-    },
-    director: {
-        name: String,
-        bio: String,
-        birth: Date
-    }
+  title: { type: String, required: true },
+  releaseUs: { type: String, required: true },
+  universe: {
+    name: String,
+    description: String,
+  },
+  director: {
+    name: String,
+    bio: String,
+    birth: Date,
+  },
 });
 
 let users_schema = mongoose.Schema({
-    name: {type: String},
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    email: {type: String, required: true},
-    birthday: Date,
-    favouriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+  name: { type: String },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  birthday: Date,
+  favouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
 
+users_schema.methods.validatePassword = function (password) {
+  return password == this.password;
+};
+
 // create models
-let Movie = mongoose.model('Movie', movies_schema);
-let User = mongoose.model('User', users_schema);
+let Movie = mongoose.model("Movie", movies_schema);
+let User = mongoose.model("User", users_schema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
