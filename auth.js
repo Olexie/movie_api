32e9fd198c22fefc95b5d3b244a757e37166a667
Olexie <116206464+Olexie@users.noreply.github.com>
2023-06-10 23:29:09 +0100

    Added jdoc documentation

--- auth.js
+++ auth.js
@@ -1,11 +1,15 @@
-const jwtSecret =
-  'my_secret_key'; /**This has to be the same key used in the JWT Strategy (in passports.js)*/
+const jwtSecret = 'my_secret_key'; //This has to be the same key used in the JWT Strategy (in passports.js)
 
 const jwt = require('jsonwebtoken'),
   passport = require('passport');
 
-require('./passport'); /**Your local pssport file, checks is the user and pass exists then is creates a JWT below if it does*/
+require('./passport'); //Your local pssport file, checks is the user and pass exists then is creates a JWT below if it does
 
+/**
+ * Generate a JWT Token
+ * @param {object} user
+ * @returns {string}
+ */
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.username,
@@ -13,8 +17,16 @@ let generateJWTToken = (user) => {
     algorithm: 'HS256',
   });
 };
-/**POST LOGIN */
+
 module.exports = (router) => {
+  /** User login
+   * @name post/login
+   * @function
+   * @memberof module:routers/app~appRouter
+   * @inner
+   * @param {object} User
+   * @returns {object} an object containing the username and password
+   * */
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       console.log({ error, user });
